81b25b9d8420a7020d566d8dc84083869cad0cc0
Tobias Schwartz <tobias.schwartz@feki.de>
2015-10-27 15:57:41 -0400

    initial project upload

---
+++ PurpleX/Assets/Scripts/FX_HexGen/FX_HexInfo.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * This script holds a Hexes position value that is used for distance calculations.
+ * & its current amount of units per player 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class FX_HexInfo : MonoBehaviour {
+	public Vector3 HexPosition;
+    // reference to all the units located at this hex
+    public GameObject[] units = new GameObject[10];
+    // whether units belong to player 1 or 2
+    // both is not possible -> fight (till only one remains)
+    public bool player1;
+}
